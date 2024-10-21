e2d39207acfb82f70372f00a828f25694ec9dfc3
Allen Audrey Kish Leyble <123635493+Krumish@users.noreply.github.com>
2024-10-21 22:02:59 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
+
+
+windows.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+}
+
+
